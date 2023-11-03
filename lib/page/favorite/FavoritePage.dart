75ce34f09bc7eea0316fb1f049951560e0c452e1
limcm <limcm@smsoftlab.co.kr>
2023-11-03 17:57:17 +0900

    Page add

---
+++ lib/page/favorite/FavoritePage.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// class FavoritePage extends StatelessWidget {
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SafeArea(
+//             child:
+//
+//
+//         )
+//     );
+//
+//   }
+//
+// }
+//
